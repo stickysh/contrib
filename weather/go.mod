0555744cf1290b5631e80b908c4b25fc8cfbc835
edkvm <eddy.kiselman@gmail.com>
2019-12-11 00:08:02 +0200

    add weather

---
+++ weather/go.mod
@@ -0,0 +1,5 @@
+module github.com/stickysh/contrib/weather
+
+go 1.12
+
+require github.com/stickysh/sticky-action-go v0.0.0-20191210220113-760e19341bbd
---
+++ weather/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/stickysh/sticky-action-go/wrapper"
+)
+
+func buildUrl(city, unitSys, apiKey string) string {
+	return fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%v&units=%v&appid=%v", city, unitSys, apiKey)
+}
+
+func Api(ep string) []byte {
+
+	c := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	res, err := c.Get(ep)
+	if err != nil {
+
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+
+	}
+
+	data, err := ioutil.ReadAll(res.Body)
+
+	return data
+}
+
+type Params struct {
+	City string `json:"city"`
+}
+
+func GetWeather(params Params) (string, error) {
+
+	apiKey := os.Getenv("API_KEY")
+	unitSys := os.Getenv("DEFAULT_UNIT_SYS")
+
+	url := buildUrl(params.City, unitSys, apiKey)
+
+	raw := Api(url)
+
+	var data struct {
+		Name string                 `json:"name"`
+		Main map[string]interface{} `json:"main"`
+	}
+
+	err := json.Unmarshal(raw, &data)
+	if err != nil {
+
+	}
+
+	return fmt.Sprintf("It's %v degrees in %v", data.Main["temp"], data.Name), nil
+}
+
+func main() {
+	wrapper.Start(GetWeather)
+}
